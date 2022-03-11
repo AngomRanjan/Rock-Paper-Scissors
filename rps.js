acbb864eb376e48702e12d116e09c61122b3d2f8
AngomRanjan <angomranjan@gmail.com>
2022-03-12 02:37:39 +0530

    edited rps.js new function game() is added to it

--- rps.js
+++ rps.js
@@ -6,11 +6,13 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection==='rock' && computerSelection==='scissors' || playerSelection==='paper' && computerSelection==='rock' || playerSelection==='scissor' && computerSelection==='paper')
     {
+        win++;
         return 'You Won! ' + playerSelection.toUpperCase()+ ' beats ' + computerSelection.toUpperCase();
         
     }
     else if (playerSelection==='rock' || playerSelection==='paper' || playerSelection==='scissors')
     {
+        lost++;
         return 'You Lost! '+ playerSelection.toUpperCase()+ ' beaten by ' + computerSelection.toUpperCase();
     }
     else {
@@ -26,6 +28,18 @@ function playRound(playerSelection, computerSelection) {
     else if(x===2) {return 'paper';}
     else {return 'scissors';}
 }
-  const playerSelection = "rock";
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+function game()
+{
+    for (let counter=1;counter<=5;counter++)
+    {   let computerSelection = computerPlay();
+        playerSelection=prompt('Round-'+counter+' Enter your Choice :',playerSelection);
+        console.log(playRound(playerSelection.toLowerCase(),computerSelection.toLowerCase()));
+    }
+    console.log(win+' -- '+lost);
+    if(win>lost) return 'Final Result : Congratulation! You Won.';
+    else if(win<lost) return 'Final Result : Sorry! You Lost.';
+    else return 'Final Result : It is a Draw!';
+}
+  let playerSelection = "rock";
+  let win=lost=0;
+  console.log(game());
