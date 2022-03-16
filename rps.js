112762fce7f0e0b6e139fc061ad3d0cfb15a5ad9
AngomRanjan <angomranjan@gmail.com>
2022-03-16 20:55:40 +0530

    activated the UI
    User input are now taken through image click eventlistener of usrWeapons.
    Output is shown in form of score in scoreboard. Currently, no restart option.
    Styles,contents and design are in raw form.

--- rps.js
+++ rps.js
@@ -1,15 +1,15 @@
 function playRound(playerSelection, computerSelection) {
     // your code here!
     if (playerSelection===computerSelection)
-    { 
+    { draw++;
         return 'It is a draw! '+ playerSelection.toUpperCase()+ ' equals ' + computerSelection.toUpperCase();
     }
-    else if (playerSelection==='rock' && computerSelection==='scissors' || playerSelection==='paper' && computerSelection==='rock' || playerSelection==='scissor' && computerSelection==='paper')
+    else if (playerSelection==='rock' && computerSelection==='scissors' || playerSelection==='paper' && computerSelection==='rock' || playerSelection==='scissors' && computerSelection==='paper')
     {win++;
         return 'You Won! ' + playerSelection.toUpperCase()+ ' beats ' + computerSelection.toUpperCase();
     }
     else if (playerSelection==='rock' || playerSelection==='paper' || playerSelection==='scissors')
-    {lost++
+    {lost++;
         return 'You Lost! '+ playerSelection.toUpperCase()+ ' beaten by ' + computerSelection.toUpperCase();
     }
     else {
@@ -26,22 +26,39 @@ function playRound(playerSelection, computerSelection) {
     else {return 'scissors';}
 }
 function game(playerSelection='rock')
-{
-    
-    for (let counter=1;counter<=1;counter++)
-    {   let computerSelection = computerPlay();
-        gRound=counter;
-        console.log('round ' +gRound+" - "+playRound(playerSelection.toLowerCase(),computerSelection.toLowerCase()));
-    }
-    if(win>lost) msg2=msg2+'Final Result : Congratulation! You Won.';
-    else if(win<lost) msg2=msg2+'Final Result : Sorry! You Lost.';
-    else return msg2=msg2+'Final Result : It is a Draw!';
-    return msg2;
+{   
+    if(win===5||lost===5)
+    {
+       if(win>lost) msg2='Final Result : Congratulation! You Won.';
+       else msg2='Final Result : Hard Luck! You Lost.';
+       document.getElementById("resultText").textContent= msg2;
+       document.getElementById("scoreTitle").textContent= "GAME-OVER! FINAL-SCORE";
+    //    document.getElementById("usrWeapons").disp
+    let child = document.getElementById("usrWeapons");
+    child.style.display='none';
+    child = document.getElementById("comWeapons");
+    child.style.display='none';
+    // child.parentNode.removeChild(child);
+    } 
+    else {
+    let computerSelection = computerPlay();
+    gRound++;
+    document.getElementById("resultText").textContent='round ' +gRound+" - "+playRound(playerSelection.toLowerCase(),computerSelection.toLowerCase());
+    document.getElementById("uScore").textContent=win;
+    document.getElementById("cScore").textContent=lost;
+    }      
 }
 //   let playerSelection = "rock";
-  let win=lost=gRound=0;
+
+  let win=lost=draw=gRound=0;    
   let msg1=msg2='';
-  console.log(game());
+  const section = document.getElementById('Rock');
+
+// Print the selected target
+document.getElementById('Rock').addEventListener('click', function(){game('rock');});
+document.getElementById('Paper').addEventListener('click', function(){game('paper');});
+document.getElementById('Scissors').addEventListener('click', function(){game('scissors');});
+//   console.log(game());//   choice.addEventListener("click",alert($(this).text()));
 //   document.getElementById("resultText").textContent = p;
 //   p=document.getElementById("uScore").textContent
  
